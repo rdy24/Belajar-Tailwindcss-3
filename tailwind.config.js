e30702d7d603cbc8a96b236efb18f932c1af72e2
rdy24 <raflizardeswandiy24@gmail.com>
2022-02-28 11:08:21 +0700

    add darkmode, animation, & layouting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 module.exports = {
   content: ["./*.{html,js}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
+      },
+      animation: {
+        "spin-slow": "spin 1s linear infinite",
+        goyang: "goyang 1s ease-in-out infinite",
+      },
+      keyframes: {
+        goyang: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
       },
     },
   },
